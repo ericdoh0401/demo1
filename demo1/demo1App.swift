939579af00f6e30aa31f70735da74b840aeda029
Eric Oh <oh.eric2004@gmail.com>
2024-01-29 17:40:26 -0800

    Initial Commit

---
+++ demo1/demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  demo1App.swift
+//  demo1
+//
+//  Created by Eric Oh on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
